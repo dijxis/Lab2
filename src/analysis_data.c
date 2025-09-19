b5c84f8e3aa34075c04188f54587a809112d9b57
NikitaProgrammist <nikita.govorov.nikita@yandex.ru>
2025-09-19 19:35:00 +0300

    add functions data analysis

---
+++ src/analysis_data.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+double nonlinear_equation (double *radioactivity, double *time, int N, double precision) {
+  double interval1 = 0, interval2 = 0;
+  scanf("%lf %lf", &interval1, &interval2);
+
+  double middle = (interval1 + interval2) / 2;
+  while (interval2 - interval1 >= precision) {
+    if (model(radioactivity, time, N, middle) == 0) {
+      return middle;
+    }
+    else if (model(radioactivity, time, N, interval1) * model(radioactivity, time, N, middle) < 0) {
+      interval2 = middle;
+      middle = (interval1 + interval2) / 2;
+    }
+    else {
+      interval1 = middle;
+      middle = (interval1 + interval2) / 2;
+    }
+  }
+  return middle;
+}
+
+double model(double *radioactivity, double *time, int N, double point) {
+  double res = 0;
+  for (size_t i = 0; i < N; i++) {
+    res += time[i] * exp(-time[i] / point) * (radioactivity[i] - exp(-time[i] / point));
+  }
+  return res;
+}
---
+++ src/headers/analysis_data.h
@@ -0,0 +1,7 @@
+#ifndef ANALYSIS_DATA_H
+#define ANALYSIS_DATA_H
+
+double nonlinear_equation (double *radioactivity, double *time, int N, double precision);
+double model(double *radioactivity, double *time, int N, double point);
+
+#endif // ANALYSIS_DATA_H
