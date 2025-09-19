c13361eea7d2578955cfe5c8d57371c05320af97
Mikhail <mikhail5768@inbox.ru>
2025-09-19 19:37:12 +0300

    Add include guard

--- src/headers/prepare_data.h
+++ src/headers/prepare_data.h
@@ -1,6 +1,11 @@
+#ifndef PREPARE_DATA_H
+#define PREPARE_DATA_H
+
 int experiment(double *radioactivity, double *time,
                double start_time, double end_time, double step);
 
 void add_noise(double *radioactivity, int N);
 
 double fdecay(double t);
+
+#endif // PREPARE_DATA_H
