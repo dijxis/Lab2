fa552ccdca04407395140d4aa9c8132828f9828d
dijxis <dima.karyshev@gmail.com>
2025-09-19 19:40:16 +0300

    add func dev_exp, dev_linear

---
+++ src/accuracy_analysis_data.c
@@ -0,0 +1,21 @@
+#include <math.h>
+
+double dev_exp(double *radioactivity, double *time,
+               int M, double decay_time)
+{
+    double sum_residuals1 = 0;
+    for (int i = 0; i < M; i++) {
+        sum_residuals1 += pow((radioactivity[i] - pow(e, -time[i] / decay_time)), 2);
+    }
+    return sqrt(sum_residuals1) / M;
+}
+
+double dev_linear(double *radioaktivity, double *time,
+                  int M, double decay_rate)
+{
+    double sum_residuals2 = 0;
+    for (int i = 0; i < M; i++) {
+        sum_residuals1 += pow((radioactivity[i] - (1 - time[i] / decay_rate)), 2);
+    }
+    return sqrt(sum_residuals2) / M;
+}
---
+++ src/headers/accuracy_analysis_data.h
@@ -0,0 +1,5 @@
+double dev_exp(double *radioactivity, double *time,
+               int M, double decay_time);
+
+double dev_linear(double *radioaktivity, double *time,
+                  int M, double decay_rate);
