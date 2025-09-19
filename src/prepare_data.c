7308a06c8996997e785a51a5307d98c9e589f55d
Mikhail <mikhail5768@inbox.ru>
2025-09-19 18:57:36 +0300

    Add empty functions

---
+++ src/prepare_data.c
@@ -0,0 +1,15 @@
+// TODO: Move prototypes to headers
+
+int experiment(double *radioactivity, double *time,
+               double start_time, double end_time, double step);
+
+void add_noise(double *radioactivity, int N);
+
+int experiment(double *radioactivity, double *time,
+               double start_time, double end_time, double step) {
+  return 0;
+}
+
+void add_noise(double *radioactivity, int N) {
+  return;
+}
