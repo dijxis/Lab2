c6951b8b3d6d1e59971506201e1c2cac036f4f92
Mikhail <mikhail5768@inbox.ru>
2025-09-19 19:31:51 +0300

    Add changes to prepare_data functions

--- src/headers/prepare_data.h
+++ src/headers/prepare_data.h
@@ -2,3 +2,5 @@ int experiment(double *radioactivity, double *time,
                double start_time, double end_time, double step);
 
 void add_noise(double *radioactivity, int N);
+
+double fdecay(double t);
--- src/prepare_data.c
+++ src/prepare_data.c
@@ -1,8 +1,36 @@
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "headers/prepare_data.h"
+
 int experiment(double *radioactivity, double *time,
                double start_time, double end_time, double step) {
-  return 0;
+  int n = 0, cur_time = 0;
+  for (n = 0, cur_time = start_time;
+       cur_time <= end_time && n < 100;
+       ++n, cur_time += step) {
+    time[n] = cur_time;
+  }
+  for (int i = 0; i < 100; ++i) {
+    radioactivity[i] = fdecay(time[i]);
+  }
+
+  return n;
 }
 
-void add_noise(double *radioactivity, int N) {
+void add_noise(double *radioactivity, int n) {
+  srand(time(NULL));
+
+  for (int i = 0; i < n; ++i) {
+    radioactivity[i] += (rand() % 2 - 1) * 0.05;
+  }
+
   return;
+}
+
+double fdecay(double t) {
+  double const betta = 3.5;
+
+  return pow(M_E, -t / betta);
 }
