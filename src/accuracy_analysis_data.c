c514c62d3d0dbf6cb6de19d361b1fb18489df59d
Mikhail <mikhail5768@inbox.ru>
2025-09-25 19:20:46 +0300

    Fix typo and change style in accuracy_analysis_data.c

--- src/accuracy_analysis_data.c
+++ src/accuracy_analysis_data.c
@@ -1,6 +1,6 @@
-#include "headers/accuracy_analysis_data.h"
-
 #include <math.h>
+
+#include "headers/accuracy_analysis_data.h"
 
 double dev_exp(double *radioactivity, double *time,
                int M, double decay_time)
@@ -17,7 +17,7 @@ double dev_linear(double *radioactivity, double *time,
 {
     double sum_residuals2 = 0;
     for (int i = 0; i < M; i++) {
-        sum_residuals1 += pow((radioactivity[i] - (1 - time[i] / decay_rate)), 2);
+        sum_residuals2 += pow((radioactivity[i] - (1 - time[i] / decay_rate)), 2);
     }
     return sqrt(sum_residuals2) / M;
 }
