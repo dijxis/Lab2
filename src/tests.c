a02b192618038ab9a06bcbf376e177af6fe1d880
NikitaProgrammist <nikita.govorov.nikita@yandex.ru>
2025-09-26 17:04:53 +0300

    refactor  printf

--- src/tests.c
+++ src/tests.c
@@ -14,8 +14,8 @@ int runTests() {
     }
     fclose(fp);
     runExperiment(params[0], params[1], params[2], params[3], params[4], params[5], params[6], &decay_time, &decay_rate, &time_differences);
-    if (!(abs(params[7] - decay_time) <= 0.5 && abs(params[8] - decay_rate) <= 0.5 && params[9] <= time_differences && time_differences <= params[10])) {
-      printf("Ошибка в тесте %d.", i);
+    if (!(abs(params[7] - decay_time) <= 0.05 && abs(params[8] - decay_rate) <= 0.05 && params[9] <= time_differences && time_differences <= params[10])) {
+      printf("Ошибка в тесте %d Должны быть значения %lf +- 0.05 %lf +- 0.05 %lf : %lf, а результат %lf %lf %lf.", i);
     }
   }
 }
